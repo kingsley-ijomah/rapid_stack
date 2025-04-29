617d69eb8c39c5f1eea9d7019390a15584740e86
Kingsley Ijomah <kingsley.ijomah@gmail.com>
2025-04-29 14:43:03 -0300

    Adding ability to populate response fields

--- lib/generators/backend-external-api/templates/response_type.rb
+++
@@ -1,8 +0,0 @@
-module Types
-  class <%= pascalCaseName %>ResponseType < Types::BaseObject
-    field :data, [ Types::<%= pascalCaseName %>Type ], null: true
-    field :message, String, null: true
-    field :errors, [ String ], null: true
-    field :http_status, Int, null: true
-  end
-end
--- lib/generators/backend-external-api/templates/type.rb
+++
@@ -1,7 +0,0 @@
-module Types
-  class <%= pascalCaseName %>Type < Types::BaseObject
-<% fields.forEach(function(field) { -%>
-    field :<%= field.name %>, <%= field.type %>, null: <%= field.nullable %>
-<% }); -%>
-  end
-end
